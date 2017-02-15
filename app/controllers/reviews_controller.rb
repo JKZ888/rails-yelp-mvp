a25b7561294a932f9c7f9d8288ce85da2b1c7ddd
= <jbcousinat@gmail.com>
2017-02-15 17:59:25 +0100

    finish exo

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,7 @@
 class ReviewsController < ApplicationController
+
+before_action :set_restaurant
+
   def index
   end
 
@@ -6,9 +9,17 @@ class ReviewsController < ApplicationController
   end
 
   def new
+    @review = Review.new(restaurant: @restaurant)
   end
 
   def create
+    @review = Review.new(review_params)
+    @review.restaurant = @restaurant
+    if @review.save
+    redirect_to restaurant_path(@restaurant)
+    else
+    render 'new'
+    end
   end
 
   def edit
@@ -19,4 +30,16 @@ class ReviewsController < ApplicationController
 
   def destroy
   end
+
+private
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
+
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,31 +1,10 @@
 Rails.application.routes.draw do
-  get 'reviews/index'
 
-  get 'reviews/show'
+root :to => 'restaurants#index'
 
-  get 'reviews/new'
-
-  get 'reviews/create'
-
-  get 'reviews/edit'
-
-  get 'reviews/update'
-
-  get 'reviews/destroy'
-
-  get 'restaurants/index'
-
-  get 'restaurants/show'
-
-  get 'restaurants/new'
-
-  get 'restaurants/create'
-
-  get 'restaurants/edit'
-
-  get 'restaurants/update'
-
-  get 'restaurants/destroy'
+  resources :restaurants do
+    resources :reviews, only: [ :new, :create ]
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
